27b0cdeb5f4f5a957f07daf6025c129d07c80206
Tamiele <tammaro_85@hotmail.it>
2024-11-25 22:53:24 +0100

    Terzo esercizio

--- src/esercizio3/ContoOnLine.java
+++ src/esercizio3/ContoOnLine.java
@@ -1,12 +1,14 @@
 package esercizio3;
 
 import esercizio2.ConsumiAuto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class ContoOnLine extends ContoCorrente {
-    private double maxPrelievo;
+    private static double maxPrelievo;
 
-
+    private final static Logger LOGGER = LoggerFactory.getLogger(ContoOnLine.class);
 
     public ContoOnLine(String titolare, double saldo, double maxP) {
         super(titolare, saldo);
@@ -22,8 +24,26 @@ public class ContoOnLine extends ContoCorrente {
     }
 
     public void preleva(double x) {
-        if (x <= maxPrelievo) {
+
+        try {
+            verificaSaldo(x);
             super.preleva(x);
+
+
+        } catch (BancaException e) {
+            LOGGER.error(e.getMessage());
+            System.out.println("Errore: " + e.getMessage());
         }
+
+
     }
+
+
+    private static void verificaSaldo(double saldo) {
+        if (saldo > maxPrelievo) {
+            throw new BancaException("Prelievo non disponibile");
+        }
+
+    }
+
 }
--- src/esercizio3/MainContoCorrente.java
+++ src/esercizio3/MainContoCorrente.java
@@ -6,5 +6,9 @@ public class MainContoCorrente {
         ContoCorrente contoUno = new ContoCorrente("Mario", 50);
         contoUno.preleva(60);
         System.out.println(contoUno.restituisciSaldo());
+
+        ContoOnLine contoDue = new ContoOnLine("Mario", 50, 50);
+        contoDue.preleva(60);
+
     }
 }
